eed4933ba173bb9f8c1e0640fec7b1aa45cc4124
vargattj <vargattjh@gmail.com>
2020-08-31 11:43:05 -0300

    fetch cadastro

--- src/Components/LoginCreate/index.js
+++ src/Components/LoginCreate/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useContext } from "react";
+import Input from "../Form/Input/Input";
+import Button from "../Form/Button/Button";
+import UseForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../UserContext";
 
 function LoginCreate() {
-  return <div>Login create</div>;
+  const username = UseForm();
+  const email = UseForm("email");
+  const password = UseForm();
+
+  const {userLogin} = useContext(UserContext);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if(response.ok){
+      userLogin(username.value, password.value)
+    }
+    console.log(response);
+  }
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastra-se</h1>
+
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" name="username" type="text" {...username} />
+        <Input label="Email" name="email" type="email" {...email} />
+        <Input label="Senha" name="password" type="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 }
 
 export default LoginCreate;
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,11 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido",
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/,
+    message:
+      "A senha deve ter no mínimo 6 caracteres e conter um caractter maísculo, um minúsculo e um digito.",
+  },
 };
 
 function UseForm(type) {
