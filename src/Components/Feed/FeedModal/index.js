41f68776db91bc18ca91dc3f627cbd89bc5ff11a
vargattj <vargattjh@gmail.com>
2020-09-03 16:16:30 -0300

    feed modal 2

--- src/Components/Feed/FeedModal/index.js
+++ src/Components/Feed/FeedModal/index.js
@@ -6,15 +6,20 @@ import Error from "../../Helper/Error";
 import Loading from "../../Helper/Loading";
 import PhotoContent from "../../Photo/PhotoContent";
 
-function FeedModal({ photo }) {
+function FeedModal({ photo, setModalPhoto }) {
   const { data, error, loading, request } = useFetch();
   useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
 
+  function handleOutSideClick(e){
+    console.log('Target', e.target);
+    console.log('Current Target', e.currentTarget);
+    if(e.target === e.currentTarget) setModalPhoto(null)
+  }
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutSideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
--- src/Components/Photo/PhotoContent/index.js
+++ src/Components/Photo/PhotoContent/index.js
@@ -9,21 +9,21 @@ function PhotoContent({ data }) {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} className={styles.img} />
-        <div className={styles.details}>
-          <div>
-            <p>
-              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-              <span className={styles.visualizacoes}>{photo.acessos}</span>
-            </p>
-            <h1 className="title">
-              <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
-            </h1>
-            <ul className={styles.attributes}>
-              <li>{photo.peso} kg</li>
-              <li>{photo.idade} anos</li>
-            </ul>
-          </div>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
         </div>
       </div>
       <PhotoComments id={photo.id} comments={comments} />
