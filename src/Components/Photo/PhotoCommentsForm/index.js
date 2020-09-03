c6878ac8993e7d715d2efc8357bd6c1827553d06
vargattj <vargattjh@gmail.com>
2020-09-03 18:16:19 -0300

    estilo comentario

--- src/Components/Photo/PhotoCommentsForm/index.js
+++ src/Components/Photo/PhotoCommentsForm/index.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import useFetch from "../../../Hooks/useFetch";
 import { ReactComponent as Enviar } from "../../../Assets/enviar.svg";
 import { COMMENT_POST } from "../../../api";
-import Error from '../../Helper/Error'
+import Error from "../../Helper/Error";
+
+import styles from "./PhotoCommentsForm.module.css";
 
 function PhotoCommentsForm({ id, setComments }) {
   const [comment, setComment] = useState("");
@@ -12,24 +14,25 @@ function PhotoCommentsForm({ id, setComments }) {
     e.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
     const { response, json } = await request(url, options);
-    if(response.ok){
-      setComment('');
-      setComments((comments) => [...comments, json])
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
     }
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
-      <Error error={error}/>
+      <Error error={error} />
     </form>
   );
 }
